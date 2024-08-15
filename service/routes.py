478a3732edee02d1e9a4ea16b37941441d11d8eb
Lito130965 <156519237+Lito130965@users.noreply.github.com>
2024-08-15 21:10:38 +0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -93,7 +93,7 @@ def update_accounts(acc_id):
     """Update account"""
     account = Account.find(acc_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{acc_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
